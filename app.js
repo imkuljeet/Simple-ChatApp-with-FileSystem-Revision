396f575c47f635eb63debc3fb421f87f217e8507
imkuljeet <iamkkashyap@gmail.com>
2024-07-18 18:06:06 +0530

    When clicked on login button,redirecting to main page

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req,res,next)=>{
+app.use('/login',(req,res,next)=>{
     console.log("Server is running");
 
     res.sendFile('login.html', { root: 'views' });
+})
+
+app.use((req,res,next)=>{
+    res.send("<h1>Working</h1>");
 })
 
 app.listen(3000);
